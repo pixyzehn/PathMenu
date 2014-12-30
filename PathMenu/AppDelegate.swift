539c7037049d676226713db31a083d9c43d4bb25
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2014-12-31 08:06:46 +0900

    Add sample

--- PathMenu/AppDelegate.swift
+++ PathMenu/AppDelegate.swift
@@ -9,13 +9,49 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, PathMenuDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.backgroundColor = UIColor.whiteColor()
+       
+        let storyMenuItemImage: UIImage = UIImage(named: "bg-menuitem")!
+        let storyMenuItemImagePressed: UIImage = UIImage(named: "bg-menuitem-highlighted")!
+        
+        let starImage: UIImage = UIImage(named: "icon-star")!
+        
+        let starMenuItem1: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+        
+        let starMenuItem2: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+
+        let starMenuItem3: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+        
+        let starMenuItem4: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+        
+        let starMenuItem5: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+
+        let starMenuItem6: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+
+        let starMenuItem7: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+
+        let starMenuItem8: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+
+        let starMenuItem9: PathMenuItem = PathMenuItem(image: storyMenuItemImage, highlightedImage: storyMenuItemImagePressed, ContentImage: starImage, highlightedContentImage:nil)
+
+        var menus: [PathMenuItem] = [starMenuItem1, starMenuItem2, starMenuItem3, starMenuItem4, starMenuItem5, starMenuItem6, starMenuItem7, starMenuItem8, starMenuItem9]
+       
+        let startItem: PathMenuItem = PathMenuItem(image: UIImage(named: "bg-addbutton"), highlightedImage: UIImage(named: "bg-addbutton-highlighted"), ContentImage: UIImage(named: "icon-plus"), highlightedContentImage: UIImage(named: "icon-plus-highlighted"))
+        
+        var menu: PathMenu = PathMenu(frame: self.window?.bounds, startItem: startItem, optionMenus: menus)
+        menu.delegate = self
+       
+        self.window?.backgroundColor = UIColor(red:0.91, green:0.89, blue:0.87, alpha:1)
+        self.window?.addSubview(menu)
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
@@ -40,7 +76,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    // PathMenuDelegate
 
+    func pathMenu(menu: PathMenu, didSelectIndex idx: Int) {
+        println("Select the index : \(idx)")
+    }
+    
+    func pathMenuWillAnimateOpen(menu: PathMenu) {
+        println("Menu will open!")
+    }
 
+    func pathMenuWillAnimateClose(menu: PathMenu) {
+        println("Menu will close!")
+    }
+    
+    func pathMenuDidFinishAnimationOpen(menu: PathMenu) {
+        println("Menu was open!")
+    }
+    
+    func pathMenuDidFinishAnimationClose(menu: PathMenu) {
+        println("Menu was closed!")
+    }
 }
 
